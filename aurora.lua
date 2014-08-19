4ad4358a6864b0fb91b2961a3a454973ab734225
Haleth <haleth@telenet.be>
2014-08-19 03:16:38 +0200

    Fix errors in latest beta build.

--- aurora.lua
+++ aurora.lua
@@ -1699,14 +1699,9 @@ Skin:SetScript("OnEvent", function(self, event, addon)
 
 		-- Battletag invite frame
 
-		for i = 1, 9 do
-			select(i, BattleTagInviteFrame.NoteFrame:GetRegions()):Hide()
-		end
-
 		F.CreateBD(BattleTagInviteFrame)
-		F.CreateBD(BattleTagInviteFrame.NoteFrame, .25)
 
-		local _, send, cancel = BattleTagInviteFrame:GetChildren()
+		local send, cancel = BattleTagInviteFrame:GetChildren()
 		F.Reskin(send)
 		F.Reskin(cancel)
 
